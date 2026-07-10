131555be1f21a7ca68bc9264b3feb6ced96e03f4
hasnaaain <hasnain.com110@gmail.com>
2026-07-10 23:12:36 +0500

    LeetCode: update cpp/two_sum.cpp

---
+++ cpp/two_sum.cpp
@@ -0,0 +1,26 @@
+// ======================================
+// LeetCode Problem: two sum
+// Language: cpp
+// Link: https://leetcode.com/problems/two-sum/
+// Synced by: LinkCode
+// Date: 7/10/2026, 11:12:34 PM
+// ======================================
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+         for (int i = 0; i < nums.size(); i++) {
+
+            for (int j = i + 1; j < nums.size(); j++) {
+
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+
+            }
+        }
+
+        return {};
+    }
+};
