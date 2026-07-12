adde08bc511f7fc13cc722e87d75b584233e9028
hasnaaain <hasnain.com110@gmail.com>
2026-07-12 21:55:02 +0500

    LeetCode: update cpp/number_of_good_pairs.cpp

---
+++ cpp/number_of_good_pairs.cpp
@@ -0,0 +1,26 @@
+// ======================================
+// LeetCode Problem: number of good pairs
+// Language: cpp
+// Link: https://leetcode.com/problems/number-of-good-pairs/
+// Synced by: LinkCode
+// Date: 7/12/2026, 9:55:01 PM
+// ======================================
+
+
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        vector<int> ans;
+        int pair = 0 ;
+        for(int i=0; i<nums.size(); i++){
+            for(int j=i+1; j< nums.size(); j++){
+                if(nums[i]== nums[j]){
+                    pair ++;
+                }
+
+            }
+
+        }
+        return pair;
+    }
+};
